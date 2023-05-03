68a33b4c22aa82cf52e7447e52c32de3843affaf
sagor995 <sagorahamed995@gmail.com>
2023-05-03 20:52:23 +0600

    Category model created

---
+++ database/migrations/2023_05_03_144836_create_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->integer('is_parent')->default(0)->comment("0=parent");
+            $table->text('image')->nullable();
+            $table->integer('status')->default(1)->comment("1=active, 0=inactive");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
