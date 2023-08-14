24160c4a8438f3d9e65888f005b395c37508c6d0
Sagor Ahamed <sagorahamed995@gmail.com>
2023-08-15 00:46:58 +0600

    Success demo invoice page generates

--- routes/web.php
+++ routes/web.php
@@ -64,14 +64,8 @@ Route::group(['prefix' => '/cart'], function () {
 
 //Checkout
 Route::get('/checkout', [PagesController::class, "checkout"])->middleware(['auth', 'verified'])->name('checkout');
-
-
-// SSLCOMMERZ Start
-Route::get('/example1', [SslCommerzPaymentController::class, 'exampleEasyCheckout']);
-Route::get('/example2', [SslCommerzPaymentController::class, 'exampleHostedCheckout']);
-
 Route::post('/pay', [SslCommerzPaymentController::class, 'index'])->name('makePayment');
-Route::post('/pay-via-ajax', [SslCommerzPaymentController::class, 'payViaAjax']);
+
 
 Route::post('/success', [SslCommerzPaymentController::class, 'success']);
 Route::post('/fail', [SslCommerzPaymentController::class, 'fail']);
